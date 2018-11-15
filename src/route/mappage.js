d1d7b6f5158a1f9464519368048adf8b3bb74c9e
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2018-11-16 02:28:27 +0300

    basic structure

---
+++ src/route/mappage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MapPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+export default MapPage;
