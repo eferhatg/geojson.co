13a0a3efc8dd2b6c75b6c1c0bf569c766429127c
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2018-11-21 23:04:09 +0300

    searchbox test file added

---
+++ src/modules/Header/SearchBox/index.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {TileSelector} from './index';
+import renderer from 'react-test-renderer';
+import * as emotion from 'emotion'
+import { createSerializer } from 'jest-emotion'
