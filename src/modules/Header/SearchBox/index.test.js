bd4a6b3fd04f5b5e61ef37fd4f988e4c6c8c18d1
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2018-12-05 15:16:31 +0300

    will changed test renderer

--- src/modules/Header/SearchBox/index.test.js
+++ src/modules/Header/SearchBox/index.test.js
@@ -1,5 +1,1 @@
-import React from 'react';
-import {TileSelector} from './index';
-import renderer from 'react-test-renderer';
-import * as emotion from 'emotion'
-import { createSerializer } from 'jest-emotion'
+import React from "react";
